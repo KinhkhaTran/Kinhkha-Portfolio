92c1afce6b0d88a73adfbb8e0b323ae8f6822b7b
Kinhkha Tran <Kkhatran@gmail.com>
2024-08-23 11:51:59 -0500

    Clickable linked projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -88,7 +88,25 @@ reset: true
 srRight.reveal('.skills-box',{delay: 100})
 srRight.reveal('.form-control',{delay: 100})
 
+const tabs = document.querySelectorAll("[data-target]");
+const tabContents = document.querySelectorAll("[data-content]");
 
+/*==================== QUALIFICATION TABS ====================*/
+tabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach((tabContent) => {
+            tabContent.classList.remove("qualification__active");
+        });
+        target.classList.add("qualification__active");
+
+        tabs.forEach((tab) => {
+            tab.classList.remove("qualification__active");
+        });
+        tab.classList.add("qualification__active");
+    });
+});
 
 /* ----- CHANGE ACTIVE LINK ----- */
 
